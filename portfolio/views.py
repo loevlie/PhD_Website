3ec22486269cf7dc2189e4a22f111f6440167b9f
Dennis Loevlie <loevliedenny@gmail.com>
2026-03-18 19:15:53 -0400

    Add /recipes page with Finnish salmon soup

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'portfolio/index.html')
+
+
+def recipes(request):
+    return render(request, 'portfolio/recipes.html')
